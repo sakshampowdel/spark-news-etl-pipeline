0bb0aa71f772c19cffbcdf2ec5d976bb2ee3cb93
Saksham Powdel <sakshampowdel@gmail.com>
2025-12-22 17:09:08 -0500

    feat: add npr scraping and generalize logic, create scraping orchestrator

--- src/extraction/scraper.py
+++ src/extraction/scraper.py
@@ -5,44 +5,33 @@ from typing import List
 
 from extraction.models import BronzeRecord
 
+def start_session(session: requests.Session, base_url: str, target_url: str) -> requests.Response:
+  # Pretend to be a real user
+  session.get(base_url, timeout=10) 
+  
+  # Now try the actual target URL
+  response = session.get(target_url, timeout=10)
 
-def scrape_reuters() -> List[BronzeRecord]:
+  # Raise exception if error
+  response.raise_for_status()
+
+  return response
+
+def scrape_reuters(session: requests.Session) -> List[BronzeRecord]:
   """
   Scrape reuters website for US news articles
 
   Returns:
     List[BronzeRecord]: A list of each article as a BronzeRecord
   """
-  # Create a session object
-  session = requests.Session()
-  
-  # Pretend to be a real agent
-  session.headers.update({
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
-    'Accept-Language': 'en-US,en;q=0.9',
-    'Referer': 'https://www.google.com/',
-    'DNT': '1'
-  })
-
-  try:
-    # Pretend to be a real user
-    session.get('https://www.reuters.com', timeout=10) 
-    
-    # Now try the actual target URL
-    response = session.get('https://www.reuters.com/world/us/', timeout=10)
-    response.raise_for_status()
-
-  except requests.RequestException as e:
-    print(f'Extraction failed with error: {e}')
-    return []
+  response = start_session(session, 'https://www.reuters.com', 'https://www.reuters.com/world/us/')
 
   soup = BeautifulSoup(response.text, 'html.parser')
 
   # Grab all articles in the feed
   articles = soup.find_all('li', attrs={'data-testid':'FeedListItem'})
 
-  results: List = []
+  results: List[BronzeRecord] = []
   for article in articles:
     # Grab the title div
     link_div = article.find(attrs={'data-testid':'TitleLink'})
@@ -56,3 +45,55 @@ def scrape_reuters() -> List[BronzeRecord]:
 
   return results
 
+def scrape_npr(session: requests.Session) -> List[BronzeRecord]:
+  response = start_session(session, 'https://www.npr.org', 'https://www.npr.org/sections/politics')
+
+  soup = BeautifulSoup(response.text, 'html.parser')
+
+  # Grab all articles in the feed
+  articles = soup.find_all('article', {'class':'item'})
+
+  results: List[BronzeRecord] = []
+  for article in articles:
+    # Grab the href link holder
+    link_a = article.find('a')
+
+    if link_a and link_a.has_attr('href'):
+      # Transform to url
+      article_url: str = str(link_a.get('href'))
+
+      record = BronzeRecord(article_url, str(article), 'NPR')
+      results.append(record)
+
+  return results
+
+def scrape_article_to_bronze() -> List[BronzeRecord]:
+  # Create a session object
+  session = requests.Session()
+  
+  # Pretend to be a real agent
+  session.headers.update({
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Referer': 'https://www.google.com/',
+    'DNT': '1'
+  })
+
+  sources = [
+    #scrape_reuters,
+    scrape_npr
+  ]
+
+  results: List[BronzeRecord] = []
+
+  for scrape_func in sources:
+    try:
+      print(f'Running scraper: {scrape_func.__name__}...')
+      data = scrape_func(session)
+      results.extend(data)
+    except Exception as e:
+      print(f'Error in {scrape_func.__name__}: {e}')
+      continue
+  
+  return results
