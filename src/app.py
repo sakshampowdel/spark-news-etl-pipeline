7c4d0fa20c291657b55a3350ad5be6c4365b9fb7
Saksham Powdel <sakshampowdel@gmail.com>
2025-12-22 17:02:27 -0500

    feat: use generic scraper instead of reuters, update output file name

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ from typing import List
 import pathlib
 
 from extraction.models import BronzeRecord, SilverRecord
-from extraction.scraper import scrape_reuters
+from extraction.scraper import scrape_article_to_bronze
 from extraction.utils import save_to_jsonl, load_bronze_records
 from transformation.cleaner import transform_bronze_to_silver
 
@@ -17,7 +17,7 @@ def run_bronze_layer(output_path: str) -> None:
     RuntimeError: If no records are created scraping the articles.
   """
   print('|--- Starting Bronze Layer ---|')
-  bronze_records: List[BronzeRecord] = scrape_reuters()
+  bronze_records: List[BronzeRecord] = scrape_article_to_bronze()
 
   if not bronze_records:
     raise RuntimeError('Error scraping bronze records')
@@ -51,15 +51,14 @@ def run_silver_layer(intput_path: str, output_path: str) -> None:
   
   save_to_jsonl(silver_records, output_path, mode='w')
 
-
 def main():
   root = pathlib.Path(__file__).parent.parent.resolve()
 
-  # '../data/bronze/reuters_us.jsonl'
-  bronze_output = str(root / 'data' / 'bronze' / 'reuters_us.jsonl')
+  # '../data/bronze/news_traffic_raw.jsonl'
+  bronze_output = str(root / 'data' / 'bronze' / 'news_traffic_raw.jsonl')
 
-  # '../data/silver/reuters_us.jsonl'
-  silver_output = str(root / 'data' / 'silver' / 'reuters_us.jsonl')
+  # '../data/silver/news_traffic_cleaned.jsonl'
+  silver_output = str(root / 'data' / 'silver' / 'news_traffic_cleaned.jsonl')
 
   #run_bronze_layer(bronze_output)
   run_silver_layer(bronze_output, silver_output)
