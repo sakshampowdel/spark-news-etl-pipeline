b37d1de423fa8b23d6b452e74654641b4cc09ac5
Saksham Powdel <sakshampowdel@gmail.com>
2025-12-21 20:57:11 -0500

    docs: add docstrings to save_to_jsonl and load_bronze_records

--- src/extraction/utils.py
+++ src/extraction/utils.py
@@ -5,6 +5,14 @@ from typing import List, Any
 from extraction.models import BronzeRecord, SilverRecord
 
 def save_to_jsonl(data: List[Any], filepath: str, mode: str = 'a') -> None:
+  """
+  Serializes a list of objects to a JSON Lines file.
+
+  Args:
+    data (List[Any]): A list of record objects that implement a .to_dict() method.
+    filepath (str): The location to save the output file to.
+    mode (str, optional): The mode of the file opener. Defaults to 'a'.
+  """
   os.makedirs(os.path.dirname(filepath), exist_ok=True)
 
   with open(filepath, mode, encoding='utf-8') as f:
@@ -14,6 +22,15 @@ def save_to_jsonl(data: List[Any], filepath: str, mode: str = 'a') -> None:
   print(f'Succesfully saved {len(data)} records to {filepath}')
 
 def load_bronze_records(filepath: str) -> List[BronzeRecord]:
+  """
+  Reads a jsonl file representation of a BronzeRecord object.
+
+  Args:
+    filepath (str): The location of the jsonl file.
+
+  Returns:
+    List[BronzeRecord]: The list of BronzeRecord objects read from the jsonl file.
+  """
   records: List[BronzeRecord] = []
 
   try:
