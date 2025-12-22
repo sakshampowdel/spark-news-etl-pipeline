0dd15ffbd1911a8227a2b29f79efcfe41d709b3c
Saksham Powdel <sakshampowdel@gmail.com>
2025-12-21 21:07:42 -0500

    docs: add docstrings to run_bronze_layer and run_silver_layer

--- src/app.py
+++ src/app.py
@@ -7,6 +7,15 @@ from extraction.utils import save_to_jsonl, load_bronze_records
 from transformation.cleaner import transform_bronze_to_silver
 
 def run_bronze_layer(output_path: str) -> None:
+  """
+  Orchestrates the scraping of news articles into structured Bronze records.
+  
+  Args:
+    output_path: The file path where the .jsonl data will be saved.
+      
+  Raises:
+    RuntimeError: If no records are created scraping the articles.
+  """
   print('|--- Starting Bronze Layer ---|')
   bronze_records: List[BronzeRecord] = scrape_reuters()
 
@@ -16,6 +25,17 @@ def run_bronze_layer(output_path: str) -> None:
   save_to_jsonl(bronze_records, output_path, mode='a')
 
 def run_silver_layer(intput_path: str, output_path: str) -> None:
+  """
+  Orchestrates the transformation of raw Bronze HTML records into 
+  structured Silver records.
+  
+  Args:
+    input_path: The file path to the raw .jsonl bronze data.
+    output_path: The file path where the cleaned .jsonl data will be saved.
+      
+  Raises:
+    RuntimeError: If no records are found in the bronze source.
+  """
   print('|--- Starting Silver Layer ---|')
   bronze_records: List[BronzeRecord] = load_bronze_records(intput_path)
 
